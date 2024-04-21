23dda3de7e396887ace723f0293da8e6402e7207
Kolade Akintoye <akintoyekolade1@gmail.com>
2024-04-21 11:37:31 +0800

    Update files

--- script.js
+++ script.js
@@ -47,3 +47,67 @@ themeToggle.addEventListener('click', function() {
         logoImage.src = 'assets/Logo_Dark.png'; // Change the logo to the dark version
     }
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+    
+    $('.scroll-to-section a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        
+        $(document).off("scroll");
+        
+        $('.scroll-to-section a').each(function () {
+            $(this).removeClass('active');
+        });
+        
+        $(this).addClass('active');
+        
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        
+        if (target.length) {
+            var width = $(window).width();
+            
+            if (width < 991) {
+                $('.menu-trigger').removeClass('active');
+                $('.header-area .nav').slideUp(200);
+            }
+            
+            $('html, body').stop().animate({
+                scrollTop: (target.offset().top) - 79
+            }, 500, 'swing', function () {
+                window.location.hash = target;
+                $(document).on("scroll", onScroll);
+            });
+        }
+    });
+});
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('.scroll-to-section a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('.scroll-to-section a').removeClass("active");
+            currLink.addClass("active");
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });
+}
