6ba2bb86eb4634d5415c1a70d3f91f0e1fadf7a0
Kolade Akintoye <akintoyekolade1@gmail.com>
2024-04-21 08:09:22 +0800

    Implemented Light/Dark mode

--- script.js
+++ script.js
@@ -21,3 +21,23 @@ function updateClock() {
   
   // Initialize the clock immediately
   updateClock();
+
+
+
+  // Theme Toggle
+
+  const logoImage = document.getElementById('logoImage');
+const themeToggle = document.getElementById('theme-toggle');
+const body = document.body;
+
+themeToggle.addEventListener('click', function() {
+    if (body.getAttribute('data-theme') === 'dark') {
+        body.setAttribute('data-theme', 'light');
+        themeToggle.innerHTML = '<img src="assets/moon-icon.svg" alt="Dark Mode">';
+        logoImage.src = 'assets/Logo_Light.png'; // Change the logo to the light version
+    } else {
+        body.setAttribute('data-theme', 'dark');
+        themeToggle.innerHTML = '<img src="assets/sun-icon.svg" alt="Light Mode">';
+        logoImage.src = 'assets/Logo_Dark.png'; // Change the logo to the dark version
+    }
+});
